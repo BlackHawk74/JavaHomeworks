f26ae6cc2dc281b84abe15229c2c862e6f4dc26c
Dmitry Bobrov <me@dbobrov.com>
2013-05-16 13:30:49 +0400

    Better implementation of LinkedBag

--- bag/src/ru/ifmo/ctddev/bobrov/task3/bag/LinkedBag.java
+++ bag/src/ru/ifmo/ctddev/bobrov/task3/bag/LinkedBag.java
@@ -1,18 +1,17 @@
 package ru.ifmo.ctddev.bobrov.task3.bag;
 
-import java.util.AbstractCollection;
-import java.util.Collection;
-import java.util.ConcurrentModificationException;
-import java.util.Iterator;
+import java.util.*;
 import java.util.function.Predicate;
 
 public class LinkedBag<E> extends AbstractCollection<E> {
-    private final Bag<DataHolder<E>> data;
-    private final DataHolder<E> head;
+    private final Map<E, Set<Wrapper<E>>> data;
+    private final Wrapper<E> head;
+    private int modCount = 0;
+    private long size = 0;
 
     public LinkedBag() {
-        head = new DataHolder<E>(null).makeSelfReference();
-        data = new Bag<>();
+        head = new Wrapper<E>(null).makeSelfReference();
+        data = new HashMap<>();
     }
 
     public LinkedBag(Collection<? extends E> collection) {
@@ -22,7 +21,7 @@ public class LinkedBag<E> extends AbstractCollection<E> {
 
     @Override
     public boolean contains(Object el) {
-        return data.contains(new DataHolder<>(el));
+        return data.containsKey(el);
     }
 
     @Override
@@ -32,7 +31,38 @@ public class LinkedBag<E> extends AbstractCollection<E> {
 
     @Override
     public boolean add(E el) {
-        return data.add(new DataHolder<>(el, head, head.getPrev()));
+        if (el == null) {
+            throw new NullPointerException();
+        }
+        modCount++;
+        size++;
+        Set<Wrapper<E>> group;
+        if (!contains(el)) {
+            group = new HashSet<>();
+            data.put(el, group);
+        } else {
+            group = data.get(el);
+        }
+        Wrapper<E> wrapper = new Wrapper<>(el, head, head.prev);
+        return group.add(wrapper);
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        Set<Wrapper<E>> group = data.get(o);
+        if (group == null) {
+            return false;
+        }
+        Iterator<Wrapper<E>> it = group.iterator();
+        Wrapper<E> removed = it.next();
+        it.remove();
+        removed.removeReferences();
+        if (group.isEmpty()) {
+            data.remove(o);
+        }
+        size--;
+        modCount++;
+        return true;
     }
 
     @Override
@@ -51,24 +81,15 @@ public class LinkedBag<E> extends AbstractCollection<E> {
     }
 
     @Override
-    public boolean remove(Object o) {
-        DataHolder<E> holder = data.removeAndGet(new DataHolder<>(o));
-        if (holder != null) {
-            holder.removeReferences();
-            return true;
-        }
-        return false;
-    }
-
-    @Override
     public void clear() {
         data.clear();
+        size = 0;
         head.makeSelfReference();
     }
 
     @Override
     public int size() {
-        return data.size();
+        return size > Integer.MAX_VALUE ? Integer.MAX_VALUE : (int) size;
     }
 
     private boolean filter(Predicate<Object> predicate) {
@@ -82,19 +103,15 @@ public class LinkedBag<E> extends AbstractCollection<E> {
         return modified;
     }
 
-    private int getModCount() {
-        return data.getModCount();
-    }
-
-    private class LinkedBagIterator implements Iterator<E> {
-        private DataHolder<E> position = head;
-        private int expectedModCount = getModCount();
+    private final class LinkedBagIterator implements Iterator<E> {
+        private Wrapper<E> position = head;
+        private int expectedModCount = modCount;
         private boolean canRemove = false;
 
         @Override
         public boolean hasNext() {
             checkModCount();
-            return position.getNext() != head;
+            return position.next != head;
         }
 
         @Override
@@ -103,7 +120,7 @@ public class LinkedBag<E> extends AbstractCollection<E> {
             if (!hasNext()) {
                 throw new IllegalStateException();
             }
-            position = position.getNext();
+            position = position.next;
             canRemove = true;
             return position.getValue();
         }
@@ -114,33 +131,39 @@ public class LinkedBag<E> extends AbstractCollection<E> {
                 throw new IllegalStateException("Remove called before next");
             }
             checkModCount();
-            if (data.removeExactly(position)) {
-                position.removeReferences();
-                position = position.prev;
-                expectedModCount = getModCount();
-                canRemove = false;
-            } else {
-                assert false;
+            E value = position.value;
+            Set<Wrapper<E>> group = data.get(value);
+            assert group != null;
+
+            group.remove(position);
+            Wrapper<E> newPosition = position.prev;
+            position.removeReferences();
+            position = newPosition;
+            if (group.isEmpty()) {
+                data.remove(value);
             }
+            size--;
+            expectedModCount = ++modCount;
+            canRemove = false;
         }
 
         private void checkModCount() {
-            if (getModCount() != expectedModCount) {
+            if (modCount != expectedModCount) {
                 throw new ConcurrentModificationException("Iterator is not valid because collection is modified");
             }
         }
     }
 
-    private static final class DataHolder<T> {
+    private static final class Wrapper<T> {
         private final T value;
-        private DataHolder<T> next;
-        private DataHolder<T> prev;
+        Wrapper<T> next;
+        Wrapper<T> prev;
 
-        public DataHolder(T value) {
+        public Wrapper(T value) {
             this(value, null, null);
         }
 
-        public DataHolder(T value, DataHolder<T> next, DataHolder<T> prev) {
+        public Wrapper(T value, Wrapper<T> next, Wrapper<T> prev) {
             this.value = value;
             this.next = next;
             this.prev = prev;
@@ -157,14 +180,6 @@ public class LinkedBag<E> extends AbstractCollection<E> {
             return value;
         }
 
-        public DataHolder<T> getNext() {
-            return next;
-        }
-
-        public DataHolder<T> getPrev() {
-            return prev;
-        }
-
         public void removeReferences() {
             if (next != null) {
                 next.prev = prev;
@@ -174,24 +189,10 @@ public class LinkedBag<E> extends AbstractCollection<E> {
             }
         }
 
-        public DataHolder<T> makeSelfReference() {
+        public Wrapper<T> makeSelfReference() {
             next = this;
             prev = this;
             return this;
-        }
-
-        @Override
-        public int hashCode() {
-            return value.hashCode();
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (!(o instanceof DataHolder)) {
-                return false;
-            }
-            DataHolder t = DataHolder.class.cast(o);
-            return value.equals(t.value);
         }
     }
 }
